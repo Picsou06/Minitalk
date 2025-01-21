8cf332e9ec355bd811eb2b08201d2b62cc76aa6e
Evan <evan.ossamlad@gmail.com>
2025-01-21 06:32:50 +0100

    work without norme

--- src/client.c
+++ src/client.c
@@ -1,25 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evdalmas <evdalmas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/21 05:44:48 by evdalmas          #+#    #+#             */
+/*   Updated: 2025/01/21 06:17:01 by evdalmas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../libft/includes/libft.h"
 #include <signal.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
-int main( int argc, char *argv[] ) {
+int send_message_size(int pid, int size)
+{
+    int i;
+    int bit;
+
+    i = 0;
+    while (i < 32)
+    {
+        bit = (size >> i) & 1;
+        if (bit == 1)
+            kill(pid, SIGUSR2);
+        else
+            kill(pid, SIGUSR1);
+        i++;
+        usleep(200);
+    }
+    return 0;
+}
+
+int send_message(int pid, char* message)
+{
+    int i;
+    int j;
+    int bit;
+    char c;
+
+    i = 0;
+    while (message[i])
+    {
+        c = message[i];
+        j = 0;
+        while (j < 8)
+        {
+            bit = (c >> j) & 1;
+            if (bit == 1)
+                kill(pid, SIGUSR2);
+            else
+                kill(pid, SIGUSR1);
+            j++;
+            usleep(200);
+        }
+        i++;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
     if (argc != 3) {
         ft_printf("Usage: %s <PID> <message>\n", argv[0]);
         return 1;
     }
     int pid = atoi(argv[1]);
     char *message = argv[2];
-    for (int i = 0; message[i]; i++) {
-        for (int j = 0; j < 8; j++) {
-            if (message[i] & (1 << j)) {
-                kill(pid, SIGUSR1);
-            } else {
-                kill(pid, SIGUSR2);
-            }
-            usleep(100);
-        }
-    }
+    message = ft_strjoin(message, "\n");
+    send_message_size(pid, ft_strlen(message));
+    send_message(pid, message);
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,23 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evdalmas <evdalmas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/21 05:44:51 by evdalmas          #+#    #+#             */
+/*   Updated: 2025/01/21 06:32:24 by evdalmas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../libft/includes/libft.h"
 #include <signal.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
-void handle_signal(int signal) {
-    if (signal == SIGUSR1) {
-        ft_printf("Received SIGUSR1\n");
-    } else if (signal == SIGUSR2) {
-        ft_printf("Received SIGUSR2\n");
+void	sig_handler(int signum)
+{
+    static int i = 0;
+    static char c = 0;
+    static int message_size = 0;
+    static int size_received = 0;
+    static char *message = NULL;
+
+    if (size_received < 32)
+    {
+        if (signum == SIGUSR1)
+            message_size = message_size | (0 << size_received);
+        else if (signum == SIGUSR2)
+            message_size = message_size | (1 << size_received);
+        size_received++;
+        if (size_received == 32)
+        {
+            message = malloc(message_size + 1);
+            if (!message)
+            {
+                ft_printf("Memory allocation failed\n");
+                exit(1);
+            }
+            message[message_size] = '\0';
+        }
+    }
+    else
+    {
+        if (i < message_size * 8)
+        {
+            if (i % 8 == 0)
+                c = 0;
+            if (signum == SIGUSR1)
+                c = c | (0 << (i % 8));
+            else if (signum == SIGUSR2)
+                c = c | (1 << (i % 8));
+            if (i % 8 == 7)
+                message[i / 8] = c;
+            i++;
+        }
+        if (i == message_size * 8)
+        {
+            ft_printf("Message received: %s\n", message);
+            free(message);
+            message = NULL;
+            i = 0;
+            c = 0;
+            message_size = 0;
+            size_received = 0;
+        }
     }
 }
 
-
 int main(void) {
     printf("Server PID: %d\n", getpid());
-    signal(SIGUSR1, handle_signal);
-    signal(SIGUSR2, handle_signal);
-    while (1) {
+    while (1)
+    {
+        signal(SIGUSR2, sig_handler);
+        signal(SIGUSR1, sig_handler);
         pause();
     }
     return 0;
