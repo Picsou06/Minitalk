4f18c303047c2b110290a6be80456360b689b56b
Evan <evan.ossamlad@gmail.com>
2025-01-30 22:38:24 +0100

    works

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: evdalmas <evdalmas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 05:44:48 by evdalmas          #+#    #+#             */
-/*   Updated: 2025/01/21 06:47:27 by evdalmas         ###   ########.fr       */
+/*   Updated: 2025/01/30 22:38:19 by evdalmas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-
-int send_message_size(int pid, int size)
-{
-    int i;
-    int bit;
-
-    i = 0;
-    while (i < 32)
-    {
-        bit = (size >> i) & 1;
-        if (bit == 1)
-            kill(pid, SIGUSR2);
-        else
-            kill(pid, SIGUSR1);
-        i++;
-        usleep(100);
-    }
-    return 0;
-}
 
 int send_message(int pid, char* message)
 {
@@ -55,7 +36,7 @@ int send_message(int pid, char* message)
             else
                 kill(pid, SIGUSR1);
             j++;
-            usleep(100);
+            usleep(500);
         }
         i++;
     }
@@ -69,7 +50,9 @@ int main(int argc, char *argv[]) {
     }
     int pid = atoi(argv[1]);
     char *message = argv[2];
-    send_message_size(pid, ft_strlen(message));
-    send_message(pid, message);
-    return 0;
+    char *temp;
+    temp = ft_strjoin(message, "\n");
+    send_message(pid, temp);
+    free(temp);
+	return 0;
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: evdalmas <evdalmas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 05:44:51 by evdalmas          #+#    #+#             */
-/*   Updated: 2025/01/30 21:57:09 by evdalmas         ###   ########.fr       */
+/*   Updated: 2025/01/30 22:27:14 by evdalmas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,73 +18,20 @@
 
 void	sig_handler(int signum)
 {
-    static int i = 0;
-    static char c = 0;
-    static int message_size = 0;
-    static int size_received = 0;
-    static char *message = NULL;
+	static char c = 0;
+	static int i = 0;
 
-    if (size_received < 32)
-    {
-        if (signum == SIGUSR1)
-            message_size = message_size | (0 << size_received);
-        else if (signum == SIGUSR2)
-            message_size = message_size | (1 << size_received);
-        size_received++;
-        if (size_received == 32)
-        {
-                message = malloc(message_size + 1);
-                if (!message)
-                {
-                    ft_printf("Memory allocation failed\n");
-                    exit(1);
-                }
-                message[message_size] = '\0';
-            }
-            else if (size_received > 32)
-            {
-                char *new_message = realloc(message, message_size + 1);
-                if (!new_message)
-                {
-                    ft_printf("Memory reallocation failed\n");
-                    free(message);
-                    exit(1);
-                }
-                message = new_message;
-                message[message_size] = '\0';
-            if (!message)
-            {
-                ft_printf("Memory allocation failed\n");
-                exit(1);
-            }
-            message[message_size] = '\0';
-        }
-    }
-    else
-    {
-        if (i < message_size * 8)
-        {
-            if (i % 8 == 0)
-                c = 0;
-            if (signum == SIGUSR1)
-                c = c | (0 << (i % 8));
-            else if (signum == SIGUSR2)
-                c = c | (1 << (i % 8));
-            if (i % 8 == 7)
-                message[i / 8] = c;
-            i++;
-        }
-        if (i == message_size * 8)
-        {
-            ft_printf("%s\n", message);
-            free(message);
-            message = NULL;
-            i = 0;
-            c = 0;
-            message_size = 0;
-            size_received = 0;
-        }
-    }
+	if (signum == SIGUSR1)
+		c |= (0 << i);
+	else if (signum == SIGUSR2)
+		c |= (1 << i);
+	i++;
+	if (i == 8)
+	{
+		ft_printf("%c", c);
+		i = 0;
+		c = 0;
+	}
 }
 
 int main(void) {
