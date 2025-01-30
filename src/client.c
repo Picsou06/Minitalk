992c8df7269432d1e6fdeb846f97ffeaf22d9b0f
Evan <evan.ossamlad@gmail.com>
2025-01-30 23:44:20 +0100

    everything works with bonus

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: evdalmas <evdalmas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 05:44:48 by evdalmas          #+#    #+#             */
-/*   Updated: 2025/01/30 22:38:19 by evdalmas         ###   ########.fr       */
+/*   Updated: 2025/01/30 23:31:15 by evdalmas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,43 +16,80 @@
 #include <string.h>
 #include <unistd.h>
 
-int send_message(int pid, char* message)
+int	send_end(int pid)
 {
-    int i;
-    int j;
-    int bit;
-    char c;
+	int		i;
+	char	c;
 
-    i = 0;
-    while (message[i])
-    {
-        c = message[i];
-        j = 0;
-        while (j < 8)
-        {
-            bit = (c >> j) & 1;
-            if (bit == 1)
-                kill(pid, SIGUSR2);
-            else
-                kill(pid, SIGUSR1);
-            j++;
-            usleep(500);
-        }
-        i++;
-    }
-    return 0;
+	i = 0;
+	c = '\0';
+	while (i < 8)
+	{
+		if (c & (1 << i))
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		i++;
+		usleep(800);
+	}
+	return (0);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        ft_printf("Usage: %s <PID> <message>\n", argv[0]);
-        return 1;
-    }
-    int pid = atoi(argv[1]);
-    char *message = argv[2];
-    char *temp;
-    temp = ft_strjoin(message, "\n");
-    send_message(pid, temp);
-    free(temp);
-	return 0;
+int	send_message(int pid, char *message)
+{
+	int		i;
+	int		j;
+	int		bit;
+	char	c;
+
+	i = 0;
+	while (message[i])
+	{
+		c = message[i];
+		j = 0;
+		while (j < 8)
+		{
+			bit = (c >> j) & 1;
+			if (bit == 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			j++;
+			usleep(800);
+		}
+		i++;
+	}
+	send_end(pid);
+	return (0);
+}
+
+void	handle_signal(int sig)
+{
+	if (sig == SIGUSR2)
+	{
+		ft_printf("Message recu\n");
+		exit(0);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int		pid;
+	char	*temp;
+
+	if (argc != 3)
+	{
+		ft_printf("Usage: %s <PID> <message>\n", argv[0]);
+		return (1);
+	}
+	pid = ft_atoi(argv[1]);
+	temp = ft_strjoin(argv[2], "\n");
+	signal(SIGUSR2, handle_signal);
+	send_message(pid, temp);
+	free(temp);
+	while (1)
+	{
+		pause();
+	}
+	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,20 +6,25 @@
 /*   By: evdalmas <evdalmas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 05:44:51 by evdalmas          #+#    #+#             */
-/*   Updated: 2025/01/30 22:27:14 by evdalmas         ###   ########.fr       */
+/*   Updated: 2025/01/30 23:32:16 by evdalmas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/includes/libft.h"
 #include <signal.h>
-#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
-void	sig_handler(int signum)
+void	send_received(int pid)
 {
-	static char c = 0;
-	static int i = 0;
+	kill(pid, SIGUSR2);
+}
+
+void	sig_handler(int signum, siginfo_t *info, void *context)
+{
+	static char	c;
+	static int	i;
+	void		context;
 
 	if (signum == SIGUSR1)
 		c |= (0 << i);
@@ -28,19 +33,25 @@ void	sig_handler(int signum)
 	i++;
 	if (i == 8)
 	{
-		ft_printf("%c", c);
+		printf("%c", c);
+		if (c == '\0')
+			send_received(info->si_pid);
 		i = 0;
 		c = 0;
 	}
 }
 
-int main(void) {
-    printf("Server PID: %d\n", getpid());
-    while (1)
-    {
-        signal(SIGUSR2, sig_handler);
-        signal(SIGUSR1, sig_handler);
-        pause();
-    }
-    return 0;
+int	main(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_sigaction = &sig_handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	printf("Server PID: %d\n", getpid());
+	while (1)
+		pause();
+	return (0);
 }
